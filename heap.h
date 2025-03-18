5bf5208f89661fb88209b8b118c227712e952141
Apilex100 <kumaraviral100@gmail.com>
2025-03-18 15:03:23 +0000

    extract heap implementation in different file

---
+++ heap.h
@@ -0,0 +1,54 @@
+#ifndef HEAP_H_
+#define HEAP_H_
+
+#include <assert.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#define UNIMPLEMENTED \
+do { \
+    fprintf(stderr, "%s:%d: %s is not implemented yet\n", \
+            __FILE__, __LINE__, __func__); \
+    abort(); \
+} while(0)
+
+#define HEAP_CAP 640000
+
+static_assert(HEAP_CAP % sizeof(intptr_t) == 0, "The heap capacity is divisible by the size of the pointer of the platform");
+extern uintptr_t heap[HEAP_CAP];
+
+#define CHUNK_LIST_CAP 1024
+
+void *heap_alloc(size_t size_bytes);
+
+void heap_free(void *ptr);
+
+void heap_collect();
+
+typedef struct
+{
+    uintptr_t *start;
+    size_t size;
+} Chunk;
+
+typedef struct
+{
+    size_t count;
+    Chunk chunks[CHUNK_LIST_CAP];
+} Chunk_List;
+
+extern Chunk_List alloced_chunks;
+extern Chunk_List freed_chunks;
+extern Chunk_List tmp_chunks;
+
+void chunk_list_insert(Chunk_List *list, void *ptr, size_t size);
+
+void chunk_list_merge(Chunk_List *dst, const Chunk_List *src);
+
+void chunk_list_dump(const Chunk_List *list);
+
+int chunk_list_find(const Chunk_List *list, uintptr_t* ptr);
+
+void chunk_list_remove(Chunk_List *list, size_t index);
+
+#endif // HEAP_H_
